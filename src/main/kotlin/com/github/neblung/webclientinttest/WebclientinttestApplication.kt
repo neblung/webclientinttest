53f30a4f018e3bf52744c7ffe39fea7bd3db7d48
Frank Neblung <frank.neblung@conet-isb.de>
2024-03-05 10:25:19 +0100

    Initial Commit
    as generated by https://start.spring.io/

---
+++ src/main/kotlin/com/github/neblung/webclientinttest/WebclientinttestApplication.kt
@@ -0,0 +1,11 @@
+package com.github.neblung.webclientinttest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WebclientinttestApplication
+
+fun main(args: Array<String>) {
+    runApplication<WebclientinttestApplication>(*args)
+}
---
+++ src/test/kotlin/com/github/neblung/webclientinttest/WebclientinttestApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.neblung.webclientinttest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebclientinttestApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
