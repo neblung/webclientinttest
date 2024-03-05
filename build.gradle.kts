bfbc12253757de8250da48e98c500a33f536972d
Frank Neblung <frank.neblung@conet-isb.de>
2024-03-05 12:06:16 +0100

    Add Test Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.2.3"
+    // sadly, we have to downgrade, because javalin is not compatible with jetty-12
+    // see https://github.com/javalin/javalin/issues/2067
+    id("org.springframework.boot") version "3.1.9"
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.22"
     kotlin("plugin.spring") version "1.9.22"
@@ -26,6 +28,10 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
+
+    testImplementation("io.javalin:javalin:6.1.3")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.8.0")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:5.8.0")
 }
 
 tasks.withType<KotlinCompile> {
